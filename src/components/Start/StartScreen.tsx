51392fcb31f27ddc4e1c4432c293ad9ca004f76a
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-12 18:17:02 -0700

    Attempted some Redux for dummy code

--- src/components/Start/StartScreen.tsx
+++ src/components/Start/StartScreen.tsx
@@ -1,14 +1,31 @@
 import "./StartScreen.css"; 
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 function StartScreen() {
     
     /* At a later time, add redirects for other game choices 
     When game starts, will need to generate a way to have others join an existing game */
+
+    interface Lobby {
+        gameId: number; 
+    }
+
+    const [state, setState] = useState({
+        gameLobbies: [{
+            gameId: 0
+        }]
+    }); 
+
     
+    const loadExistingLobbies() {
+        
+    }; 
+
     return (<div>
-        <Link to="/demo"><button>START BLACKJACK</button></Link>
+        <Link to="/game"><button>START BLACKJACK</button></Link>
+        <button>OTHER GAME</button>
         <button>OTHER GAME</button>
     </div>)
 }
