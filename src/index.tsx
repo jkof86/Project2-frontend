0d190b96ed88a3ba2ed8fb207ec4e3af251ab150
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-12 18:16:32 -0700

    Added some routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Demo from "./components/GameScreenDemo/GameDemoTBD";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<App />} />
+        <Route path = "/demo" element = {<Demo/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
