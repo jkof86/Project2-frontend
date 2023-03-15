c031751cfc1716780dac4a88d27a0b622f780b0f
TStutsman <teagandstutsman@gmail.com>
2023-03-15 15:16:22 -0400

    Changed index to reflect directory changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Game from './components/Game';
+import Game from './components/GameScreen/Game';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import Landing from './components/Landing/Landing';
