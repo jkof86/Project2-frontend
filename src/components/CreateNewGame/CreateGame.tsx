af66c183779b65da9c5bc5bff393bd30a72fbd92
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-16 13:27:11 -0700

    Removed commented out code

--- src/components/CreateNewGame/CreateGame.tsx
+++ src/components/CreateNewGame/CreateGame.tsx
@@ -8,28 +8,13 @@ interface NewGameValues {
 }
 
 function CreateNewGameForm() {
-    // const formik = useFormik({
-    //     initialValues: {
-    //         gameName: "", 
-    //         lobbyIsPrivate: false, 
-    //     },
-    //     onSubmit: values => {
-    //         //insert axios post request here later
-    //         console.log(values.gameName, values.lobbyIsPrivate);
-    //     }
-    // }); 
-
+   
     return (
-        //  <form onSubmit={formik.handleSubmit}>
-        //     <label>Enter a name for your game</label>
-        //     <input id="gameName" name="gameName" onChange={formik.handleChange} value={formik.values.gameName}/>
-        //     <label><Field type="checkbox" name="lobbyIsPrivate" value={formik.values.lobbyIsPrivate}/>PRIVATE</label>
-        //     <button className="create-game" type="submit">Submit</button>
-        // </form>
         <Formik 
             initialValues={{gameName: "", lobbyIsPrivate: false,}}
             onSubmit={async (values) => {
-                console.log(values)
+                console.log(values);
+                //insert axios post request here later
             }}>
             {({ values }) => (
                 <Form>
