75075ea152874e02a05085534814692efc517e66
TStutsman <teagandstutsman@gmail.com>
2023-03-17 15:07:32 -0400

    media queries

--- src/components/Start/Table.tsx
+++ src/components/Start/Table.tsx
@@ -1,5 +1,6 @@
 import useSound from "use-sound";
 import buttonClickSound from '../../sounds/buttonHover.mp3';
+import { FaUser } from "@react-icons/all-files/fa/FaUser";
 
 interface TableProps {
     name: string,
@@ -14,8 +15,10 @@ function Table({name, id, numberOfPlayers, handleJoin}:TableProps) {
     return (
         <button onMouseOver={() => playClickSound()} onClick={() => handleJoin(id)} key={id} className="dropdown-table" disabled={numberOfPlayers > 4}>
                 <h6 className="table-name">{name}</h6>
-                <p className="number-of-players">{numberOfPlayers} Player{numberOfPlayers > 1 ? "s" : ""}</p>
-                {numberOfPlayers > 4 && <p className="table-is-full">Full Table</p>}
+                <p className="number-of-players">
+                    {[...Array(numberOfPlayers)].map(e => <FaUser/>)}
+                </p>
+                {numberOfPlayers > 4 && <p className="table-is-full">Full</p>}
         </button>
     )
 }
