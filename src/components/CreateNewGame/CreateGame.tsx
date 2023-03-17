a0da638b4993977ef8409761e71266e6e6ad33a3
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-17 15:05:34 -0700

    Fixed font of new game form

--- src/components/CreateNewGame/CreateGame.tsx
+++ src/components/CreateNewGame/CreateGame.tsx
@@ -40,18 +40,18 @@ function CreateNewGameForm() {
     }
 
     return (
-        <div className="game-form">
+        <div className="game-form-div">
             <Formik 
             initialValues={{gameName: "", lobbyIsPrivate: false,}}
             onSubmit={(values) => {
                 handleNewGame(values);
             }}>
             {({ values }) => (
-                <Form>
+                <Form className="new-game-form">
                     <label className="name-label">Enter a name for your game</label>
                     <Field id="gameName" name="gameName" value={values.gameName}/>
                     
-                    <label><Field id="check" type="checkbox" name="lobbyIsPrivate"/>Set table to private</label>
+                    <label className="checkbox-label"><Field id="check" type="checkbox" name="lobbyIsPrivate"/>Set table to private</label>
                     <div className="button-div"><button className="create-game" type="submit">Submit</button></div>
                 </Form>    
             ) }
