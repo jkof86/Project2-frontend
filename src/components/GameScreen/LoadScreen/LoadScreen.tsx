cea272b70c69f8ea116ee5bd41384990826d384d
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-03-27 09:16:23 -0700

    Styled loadscreen

--- src/components/GameScreen/LoadScreen/LoadScreen.tsx
+++ src/components/GameScreen/LoadScreen/LoadScreen.tsx
@@ -1,12 +1,25 @@
+import "./LoadScreen.css";
+import DisconnectButton from '../Buttons/DisconnectButton';
+import { leaveGame, disconnectFromWebSocket } from '../GameTable/GameConnection';
+import { useNavigate } from 'react-router-dom';
 
-// import "./LoadScreen.css";
+function LoadScreen(props: any) {
+  const navigate = useNavigate();
 
-function LoadScreen() {
-        
-    return (
-        <div className="loadscreen">
-            <h1 className="load-header">LOADING GAME...</h1>
-        </div>)
+  const disconnect = () => {
+    props.setIsConnected(false);
+    leaveGame(props.tableId, props.playerId);
+    disconnectFromWebSocket();
+    navigate("/app");
+  }
+
+  return (
+    <div className="loadscreen">
+      <div className="loadscreen-div">
+        <h1 className="load-header">Loading next round...</h1>        
+      </div>
+      <DisconnectButton handleDisconnect={disconnect} />
+    </div>)
 }
 
 export default LoadScreen;
